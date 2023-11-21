b5b43269e78163dfbd9c0b1c06de6e9bbc26c91d
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 17:11:11 +1300

    position experiment

--- script.js
+++ script.js
@@ -8,14 +8,30 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ffff});
+const material = new THREE.MeshBasicMaterial({color: 'BlueViolet'});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
-
+let ticker = 0;
+let ticker2 = 0;
+let reverse = false;
 function animate(){
+    
     requestAnimationFrame(animate);
+    const ranNum = Math.random() * 20;
+    if(ticker<ranNum && reverse == false){
+        ticker+= .01;
+    }
+    else{
+        reverse = true;
+        ticker-= .01;
+        if(ticker <= -ranNum){
+            reverse = false
+        }
+    }
+    cube.position.x = ticker+1;
+    cube.position.y = ((ticker+1)/2);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
