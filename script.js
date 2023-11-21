8ef343297778b7a4227bae3f0e2655e89b87da0d
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 17:31:08 +1300

    create upward pointing arrow

--- script.js
+++ script.js
@@ -61,3 +61,17 @@ camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
+
+//create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({color: 'Blue'});
+
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
