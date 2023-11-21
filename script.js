8cef6784b090886b983f6ecdb10d1715baefadc9
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-11-21 17:18:18 +1300

    add webgl compatibility check

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import WebGl from 'three/addons/capabilities/WebGl.js';
+
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -32,8 +35,18 @@ function animate(){
     }
     cube.position.x = ticker+1;
     cube.position.y = ((ticker+1)/2);
+    cube.scale.y = ticker;
+    cube.scale.x = ticker+.5;
+    cube.scale.z = ticker-.5;
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-animate();
+
+if(WebGl.isWebGLAvailable()){
+    animate();
+}
+else{
+    const warning = WebGl.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+}
